62d599d18a040d26295b4a2ad20c891af88f7c13
keenroudy22 <kroudebush96@gmail.com>
2025-08-06 23:42:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const afcTeams = [
 
 const nfcTeams = rankings.filter(team => !afcTeams.includes(team));
 
-const players = ["Kinnon", "Richie", "xxxxxx", "Kenzee", "Breena", "Mariah", "Dirb", "Sean"];
+const players = ["Richie", "Keen", "xxxxxx", "Kenzee", "Breena", "Mariah", "Dirb", "Sean"];
 
 const teamLogos = {
   "Eagles": "https://a.espncdn.com/i/teamlogos/nfl/500/phi.png",
